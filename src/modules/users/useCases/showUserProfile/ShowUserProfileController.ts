104ab070f7432d7d2c336a80a59d257ad815656e
Leo <leonardolp@gmail.com>
2022-05-15 21:50:05 -0300

    Finalizado

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,7 +6,15 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+
+      const users = this.showUserProfileUseCase.execute({ user_id });
+      return response.status(201).json(users);
+    }
+    catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -6,7 +6,15 @@ class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+
+      const users = this.turnUserAdminUseCase.execute({ user_id });
+      return response.status(201).json(users);
+    }
+    catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
